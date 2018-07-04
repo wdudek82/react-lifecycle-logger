7e328fd575622219f8e5bfd4937d707b63a8cae6
Wojtek Dudek <wdudek82@gmail.com>
2018-07-05 00:56:48 +0200

    Added defaultProps static property

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 // @flow
 import React from 'react';
 
-type Props = {};
+type Props = {
+  foo?: number,
+};
 
 class App extends React.Component<Props> {
   static displayName = 'AnotherOne';
+
+  static defaultProps = {
+    foo: 5,
+  }
+
+  componentDidUpdate() {
+    console.log(this.props.foo);
+  }
 
   render() {
     return <h1>Hello</h1>;
